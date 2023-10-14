d00b344675a47fa065aadc05121f3f28a671dedc
Matheus Pereira Dias <matheuspd07@gmail.com>
2023-10-13 22:47:12 -0300

    final version without GUI

--- src/stream_client.py
+++ src/stream_client.py
@@ -3,51 +3,9 @@ import numpy as np
 import pyaudio
 import threading
 import socket
-import tkinter as tk
-from tkinter import Canvas, PhotoImage, Button
-from PIL import Image, ImageTk
-
-
-capturing = False
-def toggle_capture():
-    global capturing
-    global stream_thread
-    capturing = not capturing
-    # If user starts capturing, then start streaming video and show it on screen.
-    if capturing:
-        start_button.config(text="Stop Capture")
-        update_frame()
-        stream_thread = threading.Thread(target=stream_video)
-        stream_thread.start()
-
-    else:
-        start_button.config(text="Start Capture")
-        # Set the canvas background to black when stopping capture
-        canvas.create_image(0, 0, image=blank_photo, anchor=tk.NW)
-
-        if started_loop:
-            audio_thread.join(timeout=0)
-            video_thread.join(timeout=0)
-            stream_thread.join(timeout=0)
-        
-            cap.release()
-            root.destroy()
-            exit(0)
-
-# Function to capture webcam feed and update the canvas
-def update_frame():
-    ret, frame = cap.read()
-    if ret:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        photo = ImageTk.PhotoImage(image=Image.fromarray(frame))
-        canvas.create_image(0, 0, image=photo, anchor=tk.NW)
-        canvas.photo = photo
-    if capturing:
-        canvas.after(10, update_frame)  # Update the frame every 10 milliseconds
-
 
 # Listening to Server and Sending Nickname
-def receive(client, nickname):
+def receive(client, nickname, status):
     while True:
         try:
             # Receive Message From Server
@@ -67,9 +25,8 @@ def receive(client, nickname):
             client.close()
             break
 
-
 # Sending Messages To Server
-def write(client, nickname):
+def write(client, nickname, status):
     while True:
         message = input('')
         if message.lower() == "/quit" or message.lower() == "/exit":
@@ -79,7 +36,6 @@ def write(client, nickname):
         else:
             message = '{}: {}'.format(nickname, message)
             client.send(message.encode('ascii'))
-    
 
 # Função para capturar e enviar áudio em tempo real por uma conexão TCP
 def audio_capture_and_send(audio_stream, tcp_audio_socket, connection_status):
@@ -95,15 +51,19 @@ def audio_capture_and_send(audio_stream, tcp_audio_socket, connection_status):
             connection_status.set()  # Define a flag para encerrar o código
             break
 
-
 # Função para enviar quadros de vídeo por uma conexão TCP
 def send_video_frames(cap, tcp_video_socket, connection_status):
+    cv2.namedWindow('Local Video', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Local Video', 800, 600)
+
     while True:
         ret, frame = cap.read()
         
         if not ret:
             print("Erro ao ler quadro.")
             break
+
+        cv2.imshow('Local Video', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             connection_status.set()
@@ -122,7 +82,6 @@ def send_video_frames(cap, tcp_video_socket, connection_status):
             connection_status.set()  # Define a flag para encerrar o código
             break
 
-
 # Função para reproduzir áudio em tempo real recebido por uma conexão TCP
 def audio_receive_and_play(audio_stream, tcp_audio_socket):
     CHUNK = 1024
@@ -132,7 +91,6 @@ def audio_receive_and_play(audio_stream, tcp_audio_socket):
     while True:
         audio_data = tcp_audio_socket.recv(CHUNK)
         audio_stream.write(audio_data)
-
 
 # Função para receber e exibir quadros de vídeo por uma conexão TCP
 def receive_and_display_video(tcp_video_socket):
@@ -166,6 +124,14 @@ def receive_and_display_video(tcp_video_socket):
 
 
 def stream_video():
+    # Inicializa a captura de vídeo a partir da webcam (0 é o ID da webcam padrão)
+    cap = cv2.VideoCapture(0)
+
+    # Verifica se a captura de vídeo foi aberta com sucesso
+    if not cap.isOpened():
+        print("Erro ao abrir a câmera.")
+        exit()
+
     # Configura o endereço e a porta para enviar o vídeo por TCP
     tcp_video_target_address = ('127.0.0.1', 54321)  # Substitua pelo endereço IP do servidor
     tcp_video_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -185,7 +151,6 @@ def stream_video():
         exit()
 
     # Inicializa a captura e o envio de áudio em segundo plano
-    global audio_thread
     audio_stream = None
     audio_connection_status = threading.Event()
     audio_thread = threading.Thread(target=audio_capture_and_send, args=(audio_stream, tcp_audio_socket, audio_connection_status))
@@ -193,7 +158,6 @@ def stream_video():
     audio_thread.start()
 
     # Inicializa a captura e o envio de vídeo em segundo plano
-    global video_thread
     video_connection_status = threading.Event()
     video_thread = threading.Thread(target=send_video_frames, args=(cap, tcp_video_socket, video_connection_status))
     video_thread.daemon = True
@@ -204,6 +168,8 @@ def stream_video():
 
     # Libera a captura de vídeo e fecha a janela
     print("Conexão com o servidor encerrada.")
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 def watch_video():
@@ -233,19 +199,26 @@ def watch_video():
     client_chat.connect(('127.0.0.1', 12347))
 
     # Starting Threads For Listening And Writing in chat
+    receive_connection_status = threading.Event()
     receive_thread = threading.Thread(target=receive, args=(client_chat, nickname))
     receive_thread.start()
-
+    
+    write_connection_status = threading.Event()
     write_thread = threading.Thread(target=write, args=(client_chat, nickname))
     write_thread.start()
 
     # Inicializa a reprodução de áudio e exibição de vídeo em segundo plano
     audio_stream = None
+    audio_connection_status = threading.Event()
     audio_thread = threading.Thread(target=audio_receive_and_play, args=(audio_stream, tcp_audio_socket))
     audio_thread.daemon = True
     audio_thread.start()
 
     receive_and_display_video(tcp_video_socket)
+
+    receive_connection_status.set()
+    write_connection_status.set()
+    audio_connection_status.set()
 
     cv2.destroyAllWindows()
 
@@ -256,45 +229,7 @@ def main():
     choice = input("Digite 'stream' para streamar ou 'watch' para assistir: ")
 
     if choice == "stream":
-        # Create a Tkinter window
-        global root
-        root = tk.Tk()
-        root.title("Streaming")
-
-        # Create a Canvas widget to display the webcam feed
-        global canvas
-        canvas = Canvas(root, width=640, height=480)
-        canvas.pack()
-
-        global blank_image
-        global blank_photo
-        blank_image = Image.new("RGB", (640, 480), (0, 0, 0))
-        blank_photo = ImageTk.PhotoImage(image=blank_image)
-        canvas.create_image(0, 0, image=blank_photo, anchor=tk.NW)
-        canvas.photo = blank_photo
-
-        # Inicializa a captura de vídeo a partir da webcam (0 é o ID da webcam padrão)
-        global cap
-        cap = cv2.VideoCapture(0)
-
-        # Verifica se a captura de vídeo foi aberta com sucesso
-        if not cap.isOpened():
-            print("Erro ao abrir a câmera.")
-            exit()
-    
-        # Create a button to start/stop capturing
-        global start_button
-        start_button = Button(root, text="Start Capture", command=toggle_capture)
-        start_button.pack()
-
-        # Start the GUI main loop
-        global started_loop
-        started_loop = True
-        root.mainloop()
-
-        # Release the webcam and close the window when done
-        cap.release()
-        root.destroy()
+        stream_video()
     
     elif choice == "watch":
         watch_video()
